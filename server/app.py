87381493be5c8f189b7cea1ed331db31eb772ddd
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-09-28 01:44:06 +0300

    New commit

--- server/app.py
+++ server/app.py
@@ -3,28 +3,29 @@ from flask import Flask, request, session, jsonify, make_response
 app = Flask(__name__)
 app.json.compact = False
 
+
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
 @app.route('/sessions/<string:key>', methods=['GET'])
 def show_session(key):
-
+    
     session["hello"] = session.get("hello") or "World"
     session["goodnight"] = session.get("goodnight") or "Moon"
 
-    response = make_response(jsonify({
+   
+    response_data = {
+        'cookies': [{'mouse': request.cookies.get('mouse', 'Cookie')}],
         'session': {
-            'session_key': key,
-            'session_value': session[key],
             'session_accessed': session.accessed,
+            'session_key': key,
+            'session_value': session.get(key),
         },
-        'cookies': [{cookie: request.cookies[cookie]}
-            for cookie in request.cookies],
-    }), 200)
+    }
 
+    response = make_response(jsonify(response_data), 200)
     response.set_cookie('mouse', 'Cookie')
 
     return response
 
 if __name__ == '__main__':
     app.run(port=5555)
-    
